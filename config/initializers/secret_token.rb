808e8b694932a7462085d163585424bfae8ebee8
Gursoy Yerli <gursoy@yerli.org>
2012-10-16 14:07:11 -0500

    base app with devise

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+One::Application.config.secret_token = 'e1070d676b733ff82da6e2c412a60c2c8240650d5c290ea6832ddfc620f5a9502768c308ae387de2b60c7bf10154ae7577848627babe97eb77a74b34a2af5d77'
